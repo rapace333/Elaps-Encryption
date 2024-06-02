2a0b5aa3bf0e990ae2728bc7823c46ec9ee4fdb8
Axtorz <tiagoferreira0004pgmail.com>
2024-06-02 20:24:52 +0200

    correction fautes dans decrypt et encrypt

--- ElapsEncryption/Form1.Designer.cs
+++ ElapsEncryption/Form1.Designer.cs
@@ -71,7 +71,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(145, 37);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Encrypte";
+            this.button2.Text = "Encrypt";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.Encrypte);
             // 
@@ -87,7 +87,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(145, 37);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Decrypte";
+            this.button3.Text = "Decrypt";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.Decrypte);
             // 
